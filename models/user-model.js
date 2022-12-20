94e7a9e7607917b73673b9a6bcbf216f1f59d926
rangararazin <rangararazin@outlook.com>
2022-12-20 16:06:55 +0000

    implement getItem by id minor changes

--- models/user-model.js
+++ models/user-model.js
@@ -1,8 +1,17 @@
 const { db } = require("../firebaseconfig.js");
-const { collection, addDoc, doc, getDoc } = require("firebase/firestore");
+const {
+  collection,
+  addDoc,
+  doc,
+  getDoc,
+  setDoc,
+} = require("firebase/firestore");
 
 exports.insertUser = async (userBody) => {
-  const result = await addDoc(collection(db, "users"), userBody);
+  const result = await setDoc(
+    collection(db, "users", userBody.userId),
+    userBody
+  );
   const docreq = doc(db, "users", result.id);
   const data = await getDoc(docreq);
   return data.data();
